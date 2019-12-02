981282f114cafbaeb86dc994d98f22f6d9524aec
sarahweiss <sarahweiss@sarahs-mbp-2.home>
2019-12-01 20:09:32 -0500

    Added to main

--- OSFinalProj/src/Main.java
+++ OSFinalProj/src/Main.java
@@ -1,4 +1,22 @@
-
 public class Main {
+
+	public static void main(String[] args) {
+		Master master = new Master();
+		
+		master.addSlaves(new Slave(master));
+		master.addSlaves(new Slave(master));
+		master.addSlaves(new Slave(master));
+		master.addSlaves(new Slave(master));
+		master.addSlaves(new Slave(master));
+		
+		Client client1 = new Client(master);
+		Client client2 = new Client(master);
+		Client client3 = new Client(master);
+		
+		client1.requestSlave();
+		client2.requestSlave();
+		client3.requestSlave();
+
+	}
 
 }
