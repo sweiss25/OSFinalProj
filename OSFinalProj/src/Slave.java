b7f10229431f08a233c0e08542f08d645a7cd438
cszit <cszitwer@gmail.com>
2019-12-08 15:13:43 -0500

    The slave connects to client and gets job

--- OSFinalProj/src/Slave.java
+++ OSFinalProj/src/Slave.java
@@ -1,38 +1,61 @@
-import java.util.ArrayList;
-import java.util.Random;
+
+import java.io.*;
+import java.net.*;
 
 public class Slave {
+    private static int counter;
+    private static int portNumber;
 
-		private Master master;
-		private int slaveID;
-		private int numClients;
-		private ArrayList<Client> clients;
-		
-		Random r = new Random();
-		
-		public Slave(Master m) {
-			master= m;
-			slaveID = r.nextInt(100);
-			clients = new ArrayList<Client>();
-		}
-		
-		//increment the counter every time a Client is connected
-		public void newClient(Client c) {
-			clients.add(c);
-			numClients++;
-		}
-		
-		//when the client disconnects, the counter decrements
-		public void removeClient(Client c) {
-			clients.remove(c);
-			numClients--;
-		}
-		
-		//Master has access to the counter of the slaves
-		public int getCounter() {
-			return numClients;
-		}
-		
-		
-		
+	public static void main(String[] args) throws IOException {
+    	        
+		// Hardcode in IP and Port here if required
+    	args = new String[] {"127.0.0.1", "30121"};
+    	
+        if (args.length != 2) {
+            System.err.println(
+                "Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        portNumber = Integer.parseInt(args[1]);
+
+        try (
+            Socket clientSocket = new Socket(hostName, portNumber);
+            PrintWriter requestWriter = // stream to write text requests to server
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader responseReader= // stream to read text response from server
+                new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream())); 
+            BufferedReader stdIn = // standard input stream to get user's requests
+                new BufferedReader(
+                    new InputStreamReader(System.in))
+        ) {
+        	
+        	
+            String userInput;
+			String serverResponse;
+            while ((userInput = stdIn.readLine()) != null) {
+                requestWriter.println(userInput); // send request to server
+				serverResponse = responseReader.readLine();
+                System.out.println("SERVER RESPONDS: \"" + serverResponse + "\"");
+            }
+            
+            
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
+    }
+	
+	public int getCounter() {
+		return counter;
+	}
+	public int getPort() {
+		return portNumber;
+	}
 }
