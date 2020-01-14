2330582d61068fd7d7ab796d1f5d00ae746f858f
sarahweiss <sarahweiss@sarahs-mbp-2.home>
2020-01-13 19:54:10 -0500

    reorganized try-catch in slave

--- OSFinalProj/src/Slave.java
+++ OSFinalProj/src/Slave.java
@@ -19,6 +19,29 @@ public class Slave {
     		System.out.println("writing to master");
     		writeToMaster.println(portNum);		
     		
+    		//client opens up communication with slave
+    		try (ServerSocket slaveToClientSocket = new ServerSocket(portNum);		
+    				Socket clientSocket = slaveToClientSocket.accept();	
+    				PrintWriter writeToClient = new PrintWriter(clientSocket.getOutputStream(), true);
+    				BufferedReader readFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+    		
+    			String clientRequest;
+    			
+    			//slave reads client's statement
+    			while ((clientRequest = readFromClient.readLine()) != null) {
+    				System.out.println("The client sent: " + clientRequest);	
+    			
+    				//slave sends back client's statement in capital letters
+    				System.out.println("slave responding to client");
+    				writeToClient.println(clientRequest.toUpperCase());		
+    			}
+    		
+    		} catch (IOException e) {
+    			System.out.println(
+    					"Exception caught when trying to listen on port " + portNum + " or listening for a connection");
+    			System.out.println(e.getMessage());
+    		}
+    		
     	} catch (UnknownHostException e) {
             System.err.println("Don't know about host 127.0.0.1");
             System.exit(1);
@@ -27,28 +50,7 @@ public class Slave {
             System.exit(1);
         }
 		
-    	//client opens up communication with slave
-		try (ServerSocket slaveToClientSocket = new ServerSocket(portNum);		
-				Socket clientSocket = slaveToClientSocket.accept();	
-				PrintWriter writeToClient = new PrintWriter(clientSocket.getOutputStream(), true);
-				BufferedReader readFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
-		
-			String clientRequest;
-			
-			//slave reads client's statement
-			while ((clientRequest = readFromClient.readLine()) != null) {
-				System.out.println("The client sent: " + clientRequest);	
-			
-				//slave sends back client's statement in capital letters
-				System.out.println("slave responding to client");
-				writeToClient.println(clientRequest.toUpperCase());		
-			}
-		
-		} catch (IOException e) {
-			System.out.println(
-					"Exception caught when trying to listen on port " + portNum + " or listening for a connection");
-			System.out.println(e.getMessage());
-		}
+    	
     	
     }
     
